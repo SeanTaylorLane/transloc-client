305b55b5b2563cc1bffb650ce1ff86f6086d21cf
stl62 <stl62@rutgers.edu>
2018-07-31 12:51:55 -0400

    Working Room db without DI

--- app/src/main/java/com/seantaylorlane/translocclient/AgenciesRepository.kt
+++ app/src/main/java/com/seantaylorlane/translocclient/AgenciesRepository.kt
@@ -1,8 +1,11 @@
 package com.seantaylorlane.translocclient
 
+import android.app.Application
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
 import android.arch.persistence.room.Room
+import android.os.AsyncTask
+import android.widget.Toast
 import com.seantaylorlane.translocclient.api.TranslocModels
 import com.seantaylorlane.translocclient.api.TranslocService
 import okhttp3.OkHttpClient
@@ -13,7 +16,13 @@ import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class AgenciesRepository {
+class AgenciesRepository(val application: Application) {
+    val agenciesDao: AgenciesDao by lazy {
+        Room.databaseBuilder(application.applicationContext, Database::class.java, "database")
+                .build()
+                .agenciesDao()
+    }
+
     val translocService: TranslocService by lazy {
         val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
         val client = OkHttpClient.Builder()
@@ -34,18 +43,33 @@ class AgenciesRepository {
     }
 
     fun getAgencies(): LiveData<List<TranslocModels.AgenciesResponse.Agency>> {
+        refreshAgencies()
+        return agenciesDao.loadAll()
+    }
+
+    fun refreshAgencies() {
         val data = MutableLiveData<List<TranslocModels.AgenciesResponse.Agency>>()
         val callback = object : Callback<TranslocModels.AgenciesResponse> {
             override fun onResponse(call: Call<TranslocModels.AgenciesResponse>?, response: Response<TranslocModels.AgenciesResponse>?) {
                 // TODO: Handle error cases
-                data.value = response?.body()?.data
+                val data = response?.body()?.data
+                SaveToDatabaseAsyncTask(agenciesDao).execute(data)
             }
 
             override fun onFailure(call: Call<TranslocModels.AgenciesResponse>?, t: Throwable?) {
                 // TODO: Handle error cases
+                Toast.makeText(application.applicationContext, "Network error", Toast.LENGTH_SHORT)
             }
         }
         translocService.getAgencies().enqueue(callback)
-        return data
+    }
+
+    private class SaveToDatabaseAsyncTask(val agenciesDao: AgenciesDao) : AsyncTask<List<TranslocModels.AgenciesResponse.Agency>, Unit, Unit>() {
+        override fun doInBackground(vararg params: List<TranslocModels.AgenciesResponse.Agency>?) {
+            val data = params[0]
+            if(data != null) {
+                agenciesDao.save(data)
+            }
+        }
     }
 }
--- app/src/main/java/com/seantaylorlane/translocclient/AgenciesViewModel.kt
+++ app/src/main/java/com/seantaylorlane/translocclient/AgenciesViewModel.kt
@@ -1,11 +1,13 @@
 package com.seantaylorlane.translocclient
 
+import android.app.Application
+import android.arch.lifecycle.AndroidViewModel
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.ViewModel
 import com.seantaylorlane.translocclient.api.TranslocModels
 
-class AgenciesViewModel : ViewModel() {
-    val agenciesRepo: AgenciesRepository by lazy { AgenciesRepository() }
+class AgenciesViewModel(app: Application) : AndroidViewModel(app) {
+    val agenciesRepo: AgenciesRepository by lazy { AgenciesRepository(app) }
     val agencies: LiveData<List<TranslocModels.AgenciesResponse.Agency>> by lazy {
         agenciesRepo.getAgencies()
     }
